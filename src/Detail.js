8075ab1768ffb74fce1ad925fb5b47f20479e47c
sunny <jjohye11@gmail.com>
2020-12-07 01:16:09 +0900

    2초 뒤에 alert 부분 사라지게 만드는 코드 작성(과제)

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import "./Detail.scss";
@@ -13,6 +13,20 @@ let 제목 = styled.h4`
 `;
 
 export default function Detail({ shoes }) {
+  let [showAlert, showAlert변경] = useState(true);
+
+  useEffect(() => {
+    // 1. 컴포넌트가 보일 때 실행, 컴포넌트가 업데이트 될 때 실행 (컴포넌트 렌더링이 끝난 후 실행)
+    let timer = setTimeout(() => {
+      showAlert변경(false);
+    }, 2000); // setTimeout : 변수에 저장해서 사용 많이 한다 ! 나중에 삭제할일도 있어서.
+    // 2. 컴포넌트가 사라질 때 코드를 실행시킬 수도 있음 . 무조건 함수를 써야 함 (구 언마운트역할)
+    return function 어쩌구() {
+      //실행할 코드
+    };
+
+    // 3. 여러개를 사용하고 싶다면 여러개 써도 된다. (실행 순서는 순차적!)
+  });
   let { id } = useParams();
   let history = useHistory();
   let { title, content, price } = shoes.filter((v) => v.id === id * 1)[0];
@@ -23,9 +37,11 @@ export default function Detail({ shoes }) {
         <박스>
           <제목 className="red">제목을 써봅니다.</제목>
         </박스>
-        <div className="my-alert">
-          <p>재고가 얼마 남지 않았습니다.</p>
-        </div>
+        {showAlert ? (
+          <div className="my-alert">
+            <p>재고가 얼마 남지 않았습니다.</p>
+          </div>
+        ) : null}
         <div className="row">
           <div className="col-md-6">
             <img
