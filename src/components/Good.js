cd5f58748533ed1ee2355876ecf222675ad5e83c
sunny <jjohye11@gmail.com>
2020-12-29 23:11:22 +0900

    가격에 쉼표 추가

--- src/components/Good.js
+++ src/components/Good.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-export default function Good({ title, content, price, img }) { // props 를 해체한 것 
+export default function Good({ title, content, price, img }) {
+  // props 를 해체한 것
   return (
     <>
       <div className="col-md-4">
-        <img
-          src={img}
-          width="100%"
-          alt={"판매상품"}
-        />
+        <img src={img} width="100%" alt={"판매상품"} />
         <h4>{title}</h4>
         <p>{content}</p>
-        <p>{price}</p>
+        <p>{price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
       </div>
     </>
   );
