2e541d759c41456280225be071b51b3829a4d8c8
sunny <jjohye11@gmail.com>
2021-09-19 23:23:07 +0900

    2강끝

--- src/App.js
+++ src/App.js
@@ -1,67 +1,61 @@
 /* eslint-disable */
-import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown, Jumbotron } from "react-bootstrap";
-import "./App.css";
-import Data from "./data.js";
-import Good from "./components/Good";
-import { Link, Route, Switch } from "react-router-dom";
-import Detail from "./Detail.js";
-import axios from "axios";
-import Loader from "./Loader";
-import { 재고context } from "./state/remain";
+import React, { useState } from 'react';
+import { Navbar, Nav, NavDropdown, Jumbotron } from 'react-bootstrap';
+import './App.css';
+import Data from './data.js';
+import Good from './components/Good';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail.js';
+import axios from 'axios';
+import Loader from './Loader';
+import { 재고context } from './state/remain';
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
   let [loading, setLoading] = useState(false);
   let [clickBtnCount, setClickBtnCount] = useState(0);
-  let [재고, 재고변경] = useState([10, 11, 12]);
+  let [재고, 재고변경] = useState([10, 11, 12]); // 많은 컴포넌트들이 사용할 것 같기에 이곳에 작성
 
   return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">sunny's MALL</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>sunny's MALL</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link as={Link} to='/'>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/detail">
+            <Nav.Link as={Link} to='/detail'>
               Detail
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <div>
-            <Jumbotron className="background">
+            <Jumbotron className='background'>
               <h1>Welcome</h1>
               <p>리액트 학습을 위해 만든 쇼핑몰입니다 :)</p>
             </Jumbotron>
-            <div className="container">
+            <div className='container'>
               <재고context.Provider value={재고}>
-                <div className="row">
+                <div className='row'>
                   {shoes.map((v, i) => (
                     <Good
                       title={v.title}
                       content={v.content}
                       price={v.price}
-                      img={`https://codingapple1.github.io/shop/shoes${
-                        i + 1
-                      }.jpg`}
+                      img={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
                       key={`${v.title}${i}`}
                     />
                   ))}
@@ -69,23 +63,22 @@ function App() {
               </재고context.Provider>
               {!clickBtnCount && (
                 <button
-                  className="btn btn-primary"
+                  className='btn btn-primary'
                   onClick={() => {
                     setLoading(true);
                     axios
-                      .get("https://codingapple1.github.io/shop/data2.json")
+                      .get('https://codingapple1.github.io/shop/data2.json')
                       .then((v) => {
                         setClickBtnCount(1);
                         setLoading(false);
-                        console.log("성공", v);
+                        console.log('성공', v);
                         shoes변경([...shoes, ...v.data]);
                       })
                       .catch(() => {
                         setLoading(false);
-                        console.error("실패");
+                        console.error('실패');
                       });
-                  }}
-                >
+                  }}>
                   {loading && <Loader />}
                   더보기
                 </button>
@@ -93,10 +86,10 @@ function App() {
             </div>
           </div>
         </Route>
-        <Route path="/detail/:id">
-          <Detail shoes={shoes} />
+        <Route path='/detail/:id'>
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경} />
         </Route>
-        <Route path="/:id">
+        <Route path='/:id'>
           <div>아무거나</div>
         </Route>
       </Switch>
--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import styled from "styled-components";
-import "./Detail.scss";
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+import { Nav } from 'react-bootstrap';
 
 let 박스 = styled.div`
   padding: 20px;
@@ -12,9 +13,11 @@ let 제목 = styled.h4`
   color: ${(props) => props.색상};
 `;
 
-export default function Detail({ shoes }) {
+export default function Detail({ shoes, 재고, 재고변경 }) {
   let [showAlert, showAlert변경] = useState(true);
-  let [inputData, inputData변경] = useState("");
+  let [inputData, inputData변경] = useState('');
+
+  let [누른탭, 누른탭변경] = useState(0);
 
   useEffect(() => {
     // 1. 컴포넌트가 보일 때 실행, 컴포넌트가 업데이트 될 때 실행 (컴포넌트 렌더링이 끝난 후 실행)
@@ -22,7 +25,7 @@ export default function Detail({ shoes }) {
       showAlert변경(false);
     }, 2000); // setTimeout : 변수에 저장해서 사용 많이 한다 ! 나중에 삭제할일도 있어서.
     // 2. 컴포넌트가 사라질 때 코드를 실행시킬 수도 있음 . 무조건 함수를 써야 함 (구 언마운트역할)
-    console.log("안녕");
+    console.log('안녕');
     return function 어쩌구() {
       //실행할 코드
       clearTimeout(timer);
@@ -37,9 +40,9 @@ export default function Detail({ shoes }) {
 
   return (
     <>
-      <div className="container">
+      <div className='container'>
         <박스>
-          <제목 className="red">제목을 써봅니다.</제목>
+          <제목 className='red'>제목을 써봅니다.</제목>
         </박스>
         {inputData}
         <input
@@ -48,34 +51,72 @@ export default function Detail({ shoes }) {
           }}
         />
         {showAlert ? (
-          <div className="my-alert">
+          <div className='my-alert'>
             <p>재고가 얼마 남지 않았습니다.</p>
           </div>
         ) : null}
-        <div className="row">
-          <div className="col-md-6">
-            <img
-              src={`https://codingapple1.github.io/shop/shoes${id * 1 + 1}.jpg`}
-              width="100%"
-              alt="이미지"
-            />
+        <div className='row'>
+          <div className='col-md-6'>
+            <img src={`https://codingapple1.github.io/shop/shoes${id * 1 + 1}.jpg`} width='100%' alt='이미지' />
           </div>
-          <div className="col-md-6 mt-4">
-            <h4 className="pt-5">{title}</h4>
+          <div className='col-md-6 mt-4'>
+            <h4 className='pt-5'>{title}</h4>
             <p>{content}</p>
             <p>{price}</p>
-            <button className="btn btn-danger">주문하기</button>
+            <Info 재고={재고}></Info>
             <button
-              className="btn btn-danger"
+              className='btn btn-danger'
+              onClick={() => {
+                재고변경([9, 10, 11]);
+              }}>
+              주문하기
+            </button>
+            <button
+              className='btn btn-danger'
               onClick={() => {
                 history.goBack();
-              }}
-            >
+              }}>
               뒤로가기
             </button>
           </div>
         </div>
+
+        <Nav className='mt-5' variant='tabs' defaultActiveKey='link-0'>
+          <Nav.Item>
+            <Nav.Link
+              eventKey='link-0'
+              onClick={() => {
+                누른탭변경(0);
+              }}>
+              Active
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey='link-1'
+              onClick={() => {
+                누른탭변경(1);
+              }}>
+              Option 2
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <TabContent 누른탭={누른탭}></TabContent>
       </div>
     </>
   );
+}
+
+function Info({ 재고 }) {
+  return <p>재고: {재고[0]}</p>;
+}
+
+function TabContent({ 누른탭 }) {
+  console.log('tab');
+  if (누른탭 === 0) {
+    return <div>0번째 내용입니다.</div>;
+  } else if (누른탭 === 1) {
+    return <div>1번째 내용입니다.</div>;
+  }
 }
