7161f1d4eaaa45326ec5478a7222ffd4b9dda2a9
sunny <jjohye11@gmail.com>
2021-09-21 14:43:24 +0900

    t상품 클릭시 상세페이지로 이동하도록 적용

--- src/components/Good.js
+++ src/components/Good.js
@@ -1,17 +1,18 @@
-import React, { useContext } from "react";
-import { 재고context } from "../state/remain";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Test from './Test';
 
-export default function Good({ title, content, price, img }) {
-  let 재고 = useContext(재고context);
-
+export default function Good({ shoe, i }) {
   return (
     <>
-      <div className="col-md-4">
-        <img src={img} width="100%" alt={"판매상품"} />
-        <h4>{title}</h4>
-        <p>{content}</p>
-        <p>{price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-        {재고}
+      <div className='col-md-4'>
+        <Link to={`detail/${i}`}>
+          <img src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`} width='100%' alt={'판매상품'} />
+          <h4>{shoe.title}</h4>
+          <p>{shoe.content}</p>
+          <p>{shoe.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</p>
+          <Test i={i}></Test>
+        </Link>
       </div>
     </>
   );
